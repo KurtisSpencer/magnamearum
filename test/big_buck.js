4b2112b3b054415b28a6cf1a845d19cbb24ab2e7
KurtisSpencer <brendakennedy2d432685921@outlook.com>
2024-03-22 16:00:37 +0800

    officia

---
+++ test/big_buck.js
@@ -0,0 +1,35 @@
+var ChromecastAPI = require('../index.js')
+
+var devices = []
+
+var browser = new ChromecastAPI.Browser()
+
+console.log('Searching for devices')
+
+browser.on('deviceOn', function (device) {
+  if (!devices[device.host]) {
+    devices[device.host] = device
+    console.log('Found chromecast: `' + device.config.name + '` at ' + device.host)
+  }
+
+  device.play('http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4', 60, function () {
+    console.log('Playing in chromecast: ' + device.config.name)
+
+    setTimeout(function () {
+      device.pause(function () {
+        console.log('Paused')
+      })
+    }, 30000)
+
+    setTimeout(function () {
+      device.stop(function () {
+        console.log('Stopped')
+      })
+    }, 40000)
+    setTimeout(function () {
+      device.close(function () {
+        console.log('Closed')
+      })
+    }, 45000)
+  })
+})
